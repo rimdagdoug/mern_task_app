889d0da490a2b1b990805f640c7f1dbdb7b1c7a9
rim <rimdagdoug99@gmail.com>
2024-03-17 21:28:37 +0100

    Add task schema & Implement route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/connectDB.js";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import path from "path";
+import { log } from "console";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,21 +16,28 @@ const app = express();
 // Call connectDB to establish database connection
 connectDB();
 
+app.use(express.json());
+//Middleware
+// const logger = (req,res,next) => {
+//   console.log("Middleware ran");
+//   console.log(req.method);
+//   next();
+// };
+
+
+//Routes
+app.get("/", (req,res) => {
+  res.send("Home page")
+});
+
+// Create a task
+app.post("/api/tasks", async(req,res) =>{
+  console.log(req.body)
+  res.send("Task created")
+});
+
 const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-});
-
-const UserModel = mongoose.model("task2", userSchema);
-
-
-app.get("/getUsers", async (req, res) => {
-  const userData = await UserModel.find();
-  res.json(userData);
 });
