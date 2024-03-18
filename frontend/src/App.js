10be0670216ab34909f5eb6b41f462068cb634d8
rim <rimdagdoug99@gmail.com>
2024-03-18 23:16:27 +0100

    peoject setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import TaskList from "./components/TaskList";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className="task-container">
+        <TaskList/>
+      </div>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
